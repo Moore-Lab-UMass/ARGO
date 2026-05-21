4a8fa5fa7c7c6b2202b11c44b68bbae892b42931
mlacadie <lacadiem14@gmail.com>
2026-05-21 11:37:06 -0400

    build fix

--- src/app/submitted/[submitted]/tables/genes/GeneFilters.tsx
+++ src/app/submitted/[submitted]/tables/genes/GeneFilters.tsx
@@ -53,6 +53,7 @@ const GeneFilters: React.FC<GeneAccordianProps> = ({
 
         cellTypes?.getLinkedGenesCelltypesByAssay.forEach((item) => {
             biosamples.set(item.biosample_value, {
+                assembly: "GRCh38",
                 name: item.biosample_value,
                 ontology: item.tissue,
                 displayname: item.displayname,
@@ -76,6 +77,7 @@ const GeneFilters: React.FC<GeneAccordianProps> = ({
         compuCellTypes?.getCompuLinkedGenesCelltypes.forEach((item) => {
             if (!biosamples.has(item.biosample_value)) {
                 biosamples.set(item.biosample_value, {
+                    assembly: "GRCh38",
                     name: item.biosample_mapping ?? item.biosample_value,
                     ontology: item.tissue,
                     displayname: item.biosample_value,
